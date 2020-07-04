59316e3a155cc1f0a9d08ef470d007d110721cb7
raymond <3383047725@qq.com>
2020-07-04 11:27:37 +0800

    第一次提交

---
+++ src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,16 @@
+package com.itheima.pojo;
+
+import lombok.Data;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: Raymond
+ * @Date: 2020/07/04/11:24
+ * @Description:
+ */
+@Data
+public class User {
+    private String name;
+    private int age;
+}
